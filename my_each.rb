a8776cb845f908007c38048365080337169412f5
Kazumi Karbowski <kazkazinus@gmail.com>
2018-04-05 03:19:32 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,11 @@
 def my_each(array)
-  while i < array.length
-    yield array[i]
-    i += 1
+  if block_given?
+    i = 0
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+  else
+    puts "Hey! No block was given!"
   end
 end
